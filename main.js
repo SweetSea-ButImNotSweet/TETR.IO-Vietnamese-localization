dea6c16b3fc289257e07c85475ca63370906ec71
SweetSea <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2025-03-30 16:43:41 +0700

    Mod này, cuối cùng đã quay trở lại cuộc sống

--- main.js
+++ main.js
@@ -5,8 +5,8 @@
 // @description  TETR.IO Việt Hóa
 // @author       SweetSea
 // @match        https://tetr.io
-// @downloadURL  https://raw.githubusercontent.com/SweetSea-ButImNotSweet/TETR.IO-Vietnamese-localization/refs/heads/main/main.js
-// @updateURL    https://raw.githubusercontent.com/SweetSea-ButImNotSweet/TETR.IO-Vietnamese-localization/refs/heads/main/main.js
+// @downloadURL  https://raw.githubusercontent.com/SweetSea-ButImNotSweet/TETR.IO-Vietnamese-localization/refs/heads/main/main.user.js
+// @updateURL    https://raw.githubusercontent.com/SweetSea-ButImNotSweet/TETR.IO-Vietnamese-localization/refs/heads/main/main.user.js
 // @run-at       document-start
 // @grant        GM_addStyle
 // @grant        GM_getValue
@@ -21,11 +21,7 @@
 (function () {
     'use strict';
 
-    const delayForMs = ms => new Promise(res => setTimeout(res, ms));
-    const XML_TYPES_ALLOWED_TO_REPLACE = ["", "text"]
-
     // GHI ĐÈ `XHLHttpRequest` để có thể can thiệp cách tải file và sửa file
-    let translationIsReady = false; // bản dịch đã sẵn sàng chưa
     function extractDomainAndPath(url) {
         const regex = /https?:\/\/([^\/]+)\/([^?]+)/;
         const match = url.match(regex);
@@ -35,6 +31,96 @@
         } else {
             return null;
         }
+    }
+
+    const BASE_URL = "https://raw.githubusercontent.com/SweetSea-ButImNotSweet/TETR.IO-Vietnamese-localization/refs/heads/main";
+    const LOCALIZE_URL = `${BASE_URL}/data/localization.json5`;
+
+    const FLIES_NEED_TO_MODIFY_FROM_LOCALIZATION_DATABASE = ["js/tetrio.js"]; // Những file cần dịch, lưu ý theo mặc định: `index.html` sẽ luôn được dịch
+
+    const UPDATE_INTERVAL = 24 * 60 * 60 * 1000; // Nên cập nhật lại file sau mỗi 24h
+    const FORCE_UPDATE_IMMEDIATELY = true; // Cập nhật ngay tức thì, dùng để kiểm tra bản dịch
+    const SHOW_LOCALIZATION_STORAGE = true;
+
+
+    let STORAGE_replacements = GM_getValue("localization", {});
+    let STORAGE_lastUpdate = GM_getValue("lastUpdate", 0);
+
+    // let safeToLocalizeString = false; // Nếu biến này là `false`, thì khả năng cao là Clouldflare đang hiện trang "Checking your browser before accessing tetr.io."
+    // Hai hàm dưới đây tự động chạy luôn, một cái là sửa `index.html`, còn lại là sửa các file khác theo FILE_TO_MODIFY
+    function modifyHTML() {
+        let observer = new MutationObserver(() => {
+            if (document.documentElement.innerHTML.includes("welcome back to TETR.IO")) {
+                let modifiedHTML = translateFile("index.html", document.documentElement.innerHTML);
+                document.documentElement.innerHTML = modifiedHTML;
+                observer.disconnect();
+                // safeToLocalizeString = true;
+            }
+        });
+        observer.observe(document.documentElement, { childList: true, subtree: true });
+    };
+
+    let checkedLocalizationUpdate = false
+    function fetchLocalization() {
+        if (checkedLocalizationUpdate) return;
+
+        // Dùng để sắp xếp lại dữ liệu từ điển trước khi đem ra dùng
+        function sortTranslationData(data) {
+            for (let [file, translation] of Object.entries(data)) {
+                data[file] = Object.fromEntries(
+                    Object.entries(translation).sort(([eng1,], [eng2,]) => {
+                        // Sắp xếp cái từ điển theo độ dài của câu gốc trong tiếng Anh
+                        // Để tránh trường hợp từ dài bị ghi đè bởi từ ngắn
+                        return eng2.length - eng1.length;
+                    })
+                )
+            };
+        }
+
+        checkedLocalizationUpdate = true;
+        return new Promise(
+            (resolve) => {
+                // Lấy bản dịch + xử lí data trước
+                GM_xmlhttpRequest({
+                    method: "GET",
+                    url: LOCALIZE_URL,
+                    onload: (response) => {
+                        try {
+                            let sortedData = JSON5.parse(response.responseText);
+                            sortTranslationData(sortedData);
+                            console.log("TETR.IO Việt hóa - Đã lấy từ điển mới và sắp xếp lại:", sortedData);
+
+                            STORAGE_replacements = sortedData
+                            GM_setValue("localization")
+                            GM_setValue("lastUpdate")
+                        } catch (e) {
+                            console.error("TETR.IO Việt hóa - Có gì đó sai sai với cái file JSON rồi", e, response.responseText);
+                            throw new Error();
+                        }
+
+                        if (SHOW_LOCALIZATION_STORAGE) {
+                            console.log("TETR.IO Việt hóa - Bộ nhớ:", GM_getValue("localization", "[TRỐNG]"))
+                        };
+                    }
+                })
+
+                // Nếu data xong rồi, thì cho dịch HTML trước
+                modifyHTML();
+                resolve()
+
+            }
+        )
+
+    }
+
+    function checkLocalizationUpdate() {
+        if (
+            FORCE_UPDATE_IMMEDIATELY
+            || Date.now() - STORAGE_lastUpdate > UPDATE_INTERVAL
+        ) {
+            fetchLocalization()
+        }
+        checkLocalizationUpdate = true
     }
 
     // Ghi đè XHLHttpRequest để thêm cơ chế khóa, để có thời gian kiểm tra, và cập nhật bản dịch mới
@@ -57,23 +143,18 @@
                 if (this.responseURL) {
                     const [hostDomain, fileParameter] = extractDomainAndPath(this.responseURL);
                     console.log("TETR.IO Việt hóa - TETR.IO đang tải từ:", this.responseURL);
-                    if (hostDomain == "tetr.io") {
-                        while (!translationIsReady) {
-                            delayForMs(200);
-                        }
+                    if (hostDomain == "tetr.io" && this.status === 200) {
+                        if (FLIES_NEED_TO_MODIFY_FROM_LOCALIZATION_DATABASE.includes(fileParameter)) {
+                            if (!checkedLocalizationUpdate) fetchLocalization();
 
-                        if (this.status === 200) {
-                            if (FLIES_NEED_TO_MODIFY_FROM_LOCALIZATION_DATABASE.includes(fileParameter) && XML_TYPES_ALLOWED_TO_REPLACE.includes(this.responseType)) {
-                                let temp = translateFile(fileParameter, this.responseText);
-                                // Little hack: phải bật lại quyền writable thì mới có thể thay content :')
-                                Object.defineProperty(this, "responseText", { writable: true });
-                                this.responseText = temp;
-                            }
+                            let temp = translateFile(fileParameter, this.responseText);
+                            // Little hack: phải bật lại quyền writable thì mới có thể thay content :')
+                            Object.defineProperty(this, "responseText", { writable: true });
+                            this.responseText = temp;
                         }
                     }
-
-                    // Vẫn gọi lại hàm gốc, hàm mình chỉ sửa bản dịch khi response báo OK (200)
                 }
+                // Vẫn gọi lại hàm gốc, hàm mình chỉ sửa bản dịch khi response báo OK (200)
                 realOnLoadFunction.call(this);
             }
 
@@ -86,64 +167,8 @@
         }
     })();
 
-    const BASE_URL = "https://raw.githubusercontent.com/SweetSea-ButImNotSweet/TETR.IO-Vietnamese-localization/refs/heads/main";
-    const LOCALIZE_URL = `${BASE_URL}/data/localization.json5`;
-
-    const FLIES_NEED_TO_MODIFY_FROM_LOCALIZATION_DATABASE = ["js/tetrio.js"]; // Những file cần dịch, lưu ý theo mặc định: `index.html` sẽ luôn được dịch
-    const FILES_NEED_TO_MODIFY_FROM_LOCALIZATION_FILES = {
-        // "res/font/hun.fnt": BASE_URL + "/font/new_hun.fnt",
-    } // TODO
-
-    const UPDATE_INTERVAL = 24 * 60 * 60 * 1000; // Nên cập nhật lại file sau mỗi 24h
-    const FORCE_UPDATE_IMMEDIATELY = true; // Cập nhật ngay tức thì, dùng để kiểm tra bản dịch
-    const SHOW_LOCALIZATION_STORAGE = true;
-
-
-    let STORAGE_replacements = GM_getValue("localization", {});
-    let STORAGE_lastUpdate = GM_getValue("lastUpdate", 0);
-
-    function shouldUpdate() {
-        return FORCE_UPDATE_IMMEDIATELY || Date.now() - STORAGE_lastUpdate > UPDATE_INTERVAL;
-    }
-
-    // Dùng để sắp xếp lại dữ liệu từ điển trước khi đem ra dùng
-    function sortTranslationData(data) {
-        for (let [file, translation] of Object.entries(data)) {
-            data[file] = Object.fromEntries(
-                Object.entries(translation).sort(([eng1,], [eng2,]) => {
-                    // Sắp xếp cái từ điển theo độ dài của câu gốc trong tiếng Anh
-                    // Để tránh trường hợp từ dài bị ghi đè bởi từ ngắn
-                    return eng2.length - eng1.length;
-                })
-            )
-        };
-    }
-
-    function fetchLocalization() {
-        GM_xmlhttpRequest({
-            method: "GET",
-            url: LOCALIZE_URL,
-            onload: function (response) {
-                try {
-                    STORAGE_replacements = JSON5.parse(response.responseText);
-                    sortTranslationData(STORAGE_replacements);
-                    console.log("TETR.IO Việt hóa - Đã lấy từ điển mới và sắp xếp lại:", STORAGE_replacements);
-
-                    GM_setValue("localization", STORAGE_replacements);
-                    GM_setValue("lastUpdate", Date.now());
-                } catch (e) {
-                    console.error("TETR.IO Việt hóa - Có gì đó sai sai với cái file JSON rồi", e, response.responseText);
-                }
-
-                if (SHOW_LOCALIZATION_STORAGE) {
-                    console.log("TETR.IO Việt hóa - Bộ nhớ:", GM_getValue("localization", "[TRỐNG]"))
-                };
-            }
-        });
-    }
-
     function translateFile(filename, theirdata) {
-        // let previousFileData = GM_getValue(`previousOriginalData_${filename}`, ""); // Cache trước đó
+        // let previousFileData = GM_getValue(`previousOriginalData_${ filename } `, ""); // Cache trước đó
         // if (FORCE_UPDATE_IMMEDIATELY || (filename && STORAGE_replacements[filename] && previousFileData !== theirdata)) {
         // }
 
@@ -163,7 +188,7 @@
         return theirdata;
     }
 
-    if (shouldUpdate()) {
+    if (checkLocalizationUpdate()) {
         fetchLocalization();
     }
 
@@ -210,31 +235,11 @@
         // Rồi mới load cả hai font và ép CSS
         font.load().then((loadedFont) => {
             document.fonts.add(loadedFont);
-            GM_addStyle(`* { font-family: 'HUN', 'LocalizedFont', sans-serif !important; }`);
+            GM_addStyle(`* { font-family: 'HUN', 'LocalizedFont', sans-serif !important; } `);
             GM_setValue("fontLoaded", true);
         }).catch((err) => {
             console.error("TETR.IO Việt hóa - LỖI tải font Việt hóa:", err);
         });
-    })();
-
-    let safeToLocalizeString = false; // Nếu biến này là `false`, thì khả năng cao là Clouldflare đang hiện trang "Checking your browser before accessing tetr.io."
-    // Hai hàm dưới đây tự động chạy luôn, một cái là sửa `index.html`, còn lại là sửa các file khác theo FILE_TO_MODIFY
-    (function modifyHTML() {
-        let observer = new MutationObserver(() => {
-            if (document.documentElement.innerHTML.includes("welcome back to TETR.IO")) {
-                // Kiểm tra thử xem HTML thực sự load xong chưa, nếu chưa xong thì chờ khoảng 100ms rồi thử lại
-                // P/s: sử dụng readystatechange event
-                while (document.readyState !== "complete") {
-                    delayForMs(100);
-                }
-
-                let modifiedHTML = translateFile("index.html", document.documentElement.innerHTML);
-                document.documentElement.innerHTML = modifiedHTML;
-                observer.disconnect();
-                safeToLocalizeString = true;
-            }
-        });
-        observer.observe(document.documentElement, { childList: true, subtree: true });
     })();
 
     // (function interceptRequests() {
